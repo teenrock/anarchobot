669a59fa87a8f335ca3a5aff21caeae964942f69
Joffrey <teenrock@users.noreply.github.com>
2019-01-25 08:15:45 +0100

    Add files via upload

--- cmds/usersCmds.js
+++ cmds/usersCmds.js
@@ -14,23 +14,61 @@ function usersCmds(message, prefix, client, command) {
     }
   
   if (message.content.startsWith('!say ')) {
-    var args = message.content.split(' ');
+    var args = message.content.split(' ')
     var sayMSG = ':no_entry_sign: Ne nous prend pas pour des ânes je te prie !';
 
     if (args[1] !== '!say') {
-      let answer = args.slice(1).join(' ');
+      let answer = args.slice(1).join(' ')
       
       if (args == null) {
         message.channel.send(`${message.author.username} vous n'avez saisi aucun argument.`)
-      } else message.channel.send(answer);
+      } else message.channel.send(answer)
 
     } else {
-      message.author.send(sayMSG);
-      message.channel.send(sayMSG).then(message => message.delete(3000));
+      message.author.send(sayMSG)
+      message.channel.send(sayMSG).then(message => message.delete(3000))
     
     }
   
   }
+
+  if (message.content.startsWith(prefix + "purge")) {
+    
+    if (message.channel.type === "dm") return
+    
+    if(!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) return message.reply("**:x: Vous n'avez pas la permission `manage-guild` dans ce serveur**").catch(console.error)
+
+    const user = message.mentions.users.first()
+    const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2]) 
+    
+    if (!amount) return message.reply('**:x: Veuillez spécifier une limite de message**')
+    
+    if (!amount && !user) 
+    
+    return message.reply('**:x: Veuillez spécifier une limite de message**')
+    
+    if (!user){
+      if(isNaN(message.content.split(' ')[1]) || parseInt(message.content.split(' ')[1]) < 2 || parseInt(message.content.split(' ')[1]) > 100){
+        message.channel.send('**:x: Veuillez spécifier une limite de message comprise entre 2 et 100**')
+      }
+    }
+
+    if(message.content.split(' ')[2]){
+      if(isNaN(message.content.split(' ')[2]) || parseInt(message.content.split(' ')[2]) < 2 || parseInt(message.content.split(' ')[2]) > 100){
+        message.channel.send('**:x: Veuillez spécifier une limite de message comprise entre 2 et 100**')
+      }
+    }
+
+    message.channel.fetchMessages({ limit: amount, }).then((messages) => {
+      if (user) {
+        const filterBy = user ? user.id : Client.user.id;
+        messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount)
+      }
+
+      message.channel.bulkDelete(messages).catch(error => console.log(error.stack))
+      message.channel.send(":wastebasket: | `" + amount + "` messages supprimés")
+   })
+}
 
 }
 
